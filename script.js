6790ea053364f4f45494abdefda09cf1d6dfe935
Victoria <vigi2586@stud.kea.dk>
2021-02-08 15:45:01 +0100

    maybe halfway

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 "use strict";
 
-document.addEventListener("DOMcontentLoaded", readInput);
+document.addEventListener("DOMContentLoaded", ButtonClick);
 
-function readInput();
+let firstnumber;
+let secondnumber;
+let calculate;
+let operator;
 
-function readRoundto();
+function ButtonClick() {
 
-function readDropdown();
+    console.log("working");
 
-function math();
+  //click calculate
+  document.querySelector("#calculate").addEventListener("click", result);
+
+  //click clear results
+  document.querySelector("#clear").addEventListener("click", ResultClear);
+
+};
+
+function result() {
+
+    console.log("done");
+
+    //read first and second number
+    firstnumber = document.querySelector("#firstnumber").value;
+    secondnumber = document.querySelector("#secondnumber").value;
+    operator = document.querySelector("#operator").value;
+
+    
+
+
+};
+
+function ResultClear() {
+
+    console.log("gone");
+
+    //clear results
+    document.querySelector("#results").innerHTML = "";
+
+};
 
