9fd5ce255a0c7ebf0a02edfb5f1258a6afa2b498
Victoria <vigi2586@stud.kea.dk>
2021-02-08 16:32:11 +0100

    Javascript done

--- script.js
+++ script.js
@@ -8,37 +8,49 @@ let calculate;
 let operator;
 
 function ButtonClick() {
-
-    console.log("working");
+  console.log("working");
 
   //click calculate
   document.querySelector("#calculate").addEventListener("click", result);
 
   //click clear results
   document.querySelector("#clear").addEventListener("click", ResultClear);
-
-};
+}
 
 function result() {
+  console.log("done");
 
-    console.log("done");
+  //read first and second number
+  firstnumber = document.querySelector("#firstnumber").value;
+  secondnumber = document.querySelector("#secondnumber").value;
 
-    //read first and second number
-    firstnumber = document.querySelector("#firstnumber").value;
-    secondnumber = document.querySelector("#secondnumber").value;
-    operator = document.querySelector("#operator").value;
+  //+  *  /  -
+  let op = document.querySelector("#operator");
+  operator = op.options[op.selectedIndex].text;
 
-    
+  //calculate
+  calculate = eval(firstnumber + operator + secondnumber);
 
+  //rounded box checked off? -> yes/no
+  if (document.querySelector("#doround").checked == true) {
+    let round = document.querySelector("#decimals");
+    let roundNum = round.options[round.selectedIndex].text;
+    calculate = calculate.toFixed(roundNum);
+  }
 
-};
+  //write result
+  document.querySelector("#results").innerHTML += "<li>" + calculate + "</li>";
+
+  //write result in 'firstnumber'
+  document.querySelector("#firstnumber").value = calculate;
+
+  //scroll list to bottom
+  document.querySelector("#results").lastChild.scrollIntoView(false);
+}
 
 function ResultClear() {
+  console.log("gone");
 
-    console.log("gone");
-
-    //clear results
-    document.querySelector("#results").innerHTML = "";
-
-};
-
+   //clear results
+  document.querySelector("#results").innerHTML = "";
+}
